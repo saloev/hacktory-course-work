314de900595423673ee4f4ab9dadf6a6a625250a
saloev <saloev.saadi@yandex.ru>
2019-12-23 19:39:59 +0300

    add auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,48 @@
-import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
+import store from '@/store/index';
+import routes from '@/router/routes';
 
 const router = new VueRouter({
+  linkExactActiveClass: 'active',
   mode: 'history',
-  base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const loaded = { user: false };
+  const setLoaded = (mode) => {
+    loaded[mode] = true;
+    if (loaded.user) next();
+  };
+
+  if (to.meta && to.meta.auth) {
+    store.dispatch('route', to);
+    const params = Object.assign({}, { name: to.meta.api, params: to.params }, to.meta);
+    store
+      .dispatch('AuthUser', params)
+      .then(() => {
+        setLoaded('page');
+      })
+      .catch((err) => {
+        // throw new Error(err);
+        console.warn(err);
+        // next();
+        next({
+          path: '/',
+        });
+      });
+  } else setLoaded('user');
+});
+
+router.afterEach(() => {
+  setTimeout(() => {
+    window.scrollTo({
+      behavior: 'auto', // 'smooth',
+      left: 0,
+      top: 0,
+    });
+  }, 500);
 });
 
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,25 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: null,
   },
   mutations: {
+    saveUserData(state, payload) {
+      state.user = payload;
+    },
   },
   actions: {
+    userAuth({ commit }, payload) {
+      const url = new URL('http://localhost:3001');
+      const response = fetch(url,
+        {
+          method: 'POST',
+          body: payload,
+        });
+      const res = response.json();
+
+      commit('saveUserData', res);
+    },
   },
   modules: {
   },
