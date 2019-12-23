00da7d56903ef59e9242dbd0f1bc2a7089bc400a
saloev <saloev.saadi@yandex.ru>
2019-12-23 18:43:41 +0300

    add auth form

---
+++ README.md
@@ -0,0 +1,9 @@
+# xss-sql-injection
+
+## Example of xss and sql injection
+
+### About what this web site ?
+
+### xss
+
+### sql injection
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <RegForm msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RegForm from '@/components/RegForm.vue';
+
+export default {
+  name: 'home',
+  components: {
+    RegForm,
+  },
+};
+</script>
